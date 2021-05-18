845d265a1cb4022454e6462778d33f94cd044d57
maodm <maodm@yunphant.com>
2021-05-18 13:46:47 +0800

    添加了布署合约相关的node模块

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-
+require("@nomiclabs/hardhat-ethers");
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async () => {
